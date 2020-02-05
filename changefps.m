1a68d8684c9a8af8f6a11ddbb91745a2d353dfb4
Fengjing <yukuns@163.com>
2020-02-05 17:27:12 +0800

    change the video fps by matlab

---
+++ changefps.m
@@ -0,0 +1,8 @@
+%% change fps
+%% Yk S
+close all;clear all;clc;
+video_file='1_14.35.42.avi';
+M = aviread(video_file);
+%%%%%%%%%%%%%%% change fps
+fps = 25;
+movie2avi(M,[video_file,'_fps',num2str(fps),'.avi'],'compression', 'None','FPS',fps);
