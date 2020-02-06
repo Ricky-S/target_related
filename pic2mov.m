64e4e2c7d9e09ff86f2a25ac62cba60c027f9413
Fengjing <yukuns@163.com>
2020-02-06 20:50:57 +0800

    add instructions

--- pic2mov.m
+++ pic2mov.m
@@ -1,10 +1,12 @@
-
-close all;clear all;clc;
-for i=1:148
-    namein=['target',num2str(i),'.jpg'];
-    pic=imread(namein);
-    im=double(pic);
-    mov(i)=im2frame(uint8(im)); 
+%%Yk S
+close all;
+clear all;
+clc;
+for i = 1:148
+    namein = ['target',num2str(i),'.jpg'];
+    pic = imread(namein);
+    im = double(pic);
+    mov(i) = im2frame(uint8(im)); 
 end 
-aviname=['1_14.37.42.avi.target_fps5.avi'];
-movie2avi(mov,aviname,'FPS',5,'COMPRESSION' ,'NONE','quality',100);
+aviname = ['1_14.37.42.avi.target_fps5.avi'];
+movie2avi(mov , aviname , 'FPS' , 5 , 'COMPRESSION' , 'NONE' , 'quality',100);
